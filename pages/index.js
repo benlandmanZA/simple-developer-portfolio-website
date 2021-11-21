fbb68a64bfa1a272078511bc1223fbd24985b3c3
AMALI-bit <55197327+AMALI-bit@users.noreply.github.com>
2021-11-22 00:19:44 +0200

    Removed un needed pages from template.

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,11 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import React from "react";
 import ContainerBlock from "../components/ContainerBlock";
-import FavouriteProjects from "../components/FavouriteProjects";
-import LatestCode from "../components/LatestCode";
-import Hero from "../components/Hero";
-import getLatestRepos from "@lib/getLatestRepos";
-import userData from "@constants/data";
+import AboutMe from "../components/AboutMe";
 
-export default function Home({ repositories }) {
+export default function about() {
   return (
-    <ContainerBlock
-      title="Manu Arora - Developer, Writer, Creator"
-      description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
-    >
-      <Hero />
-      <FavouriteProjects />
-      <LatestCode repositories={repositories} />
+    <ContainerBlock>
+      <AboutMe />
     </ContainerBlock>
   );
 }
-
-export const getServerSideProps = async () => {
-  console.log(process.env.GITHUB_AUTH_TOKEN);
-  let token = process.env.GITHUB_AUTH_TOKEN;
-
-  const repositories = await getLatestRepos(userData, token);
-  // console.log("REPOSITORIES", repositories);
-
-  return {
-    props: {
-      repositories,
-    },
-  };
-};
